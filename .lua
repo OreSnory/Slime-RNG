61838c740369f2ace8005fd27a1795cd7694aef9
OreSnory <oren.sincock@gmail.com>
2026-05-16 22:42:52 +1200

    Update .lua

--- .lua
+++ .lua
@@ -31,14 +31,19 @@ local Section = Main:CreateSection("AutoFarm")
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+-- Variables
+local LocalPlayer = Players.LocalPlayer
+local RollCoolDown = LocalPlayer.PlayerGui.Root.BottomBarStats.StatsList.RollSpeedStat.Content.Value.TextLabel.Text:match("[%d%.]+")
+
+-- Modules
+local client = require(ReplicatedStorage.Packages.DataService).client
+local UpgradeTree = require(ReplicatedStorage.Source.Features.Upgrades.UpgradeTree)
+local UpgradeUtils = require(ReplicatedStorage.Source.Features.Upgrades.UpgradeServiceUtils)
+
 -- Remotes
 local Roll = ReplicatedStorage:WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("leifstout_networker@0.3.1"):WaitForChild("networker"):WaitForChild("_remotes"):WaitForChild("RollService"):WaitForChild("RemoteFunction")
 local EquipBest = ReplicatedStorage:WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("leifstout_networker@0.3.1"):WaitForChild("networker"):WaitForChild("_remotes"):WaitForChild("InventoryService"):WaitForChild("RemoteFunction")
-
--- Variables
-local LocalPlayer = Players.LocalPlayer
-
-local RollCoolDown = LocalPlayer.PlayerGui.Root.BottomBarStats.StatsList.RollSpeedStat.Content.Value.TextLabel.Text:match("[%d%.]+")
+local BuyUpgrade = ReplicatedStorage:WaitForChild("Packages"):WaitForChild("_Index"):WaitForChild("leifstout_networker@0.3.1"):WaitForChild("networker"):WaitForChild("_remotes"):WaitForChild("UpgradeService"):WaitForChild("RemoteFunction")
 
 local Toggle = Main:CreateToggle({
     Name = "Auto Roll",
@@ -76,6 +81,31 @@ Main:CreateToggle({
                 AutoEquipBestConn:Disconnect()
                 AutoEquipBestConn = nil
             end
+        end
+    end
+})
+
+local Toggle = Main:CreateToggle({
+    Name = "Auto Buy Upgrades",
+    CurrentValue = false,
+    Flag = "AutoBuyUpgrades",
+    Callback = function(Value)
+        AutoBuyUpgrades = Value
+
+        if AutoBuyUpgrades then
+            task.spawn(function()
+                while AutoBuyUpgrades do
+                    for _, Upgrades in next, UpgradeTree do
+                        for _, Tiles in next, Upgrades do
+                            if Tiles.cost and not UpgradeUtils.ownsUpgrade(Tiles.id, client:get("upgrades") or {}) and (client:get()[Tiles.cost.currency] or 0) >= Tiles.cost.amount then
+                                BuyUpgrade:InvokeServer("requestUnlock", Tiles.id)
+                                task.wait(1)
+                            end
+                        end
+                    end
+                    task.wait(1)
+                end
+            end)
         end
     end
 })
